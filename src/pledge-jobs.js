edb7bc0dc4421b67da816e19d504dc18af2faa48
Nicholas C. Zakas <nicholas@nczconsulting.com>
2020-09-11 10:51:05 -0700

    Chore: Add in more spec references

--- src/pledge-jobs.js
+++ src/pledge-jobs.js
@@ -10,15 +10,29 @@
 import { createResolvingFunctions } from "./pledge.js";
 
 //-----------------------------------------------------------------------------
-// Exports
+// 8.4.1 HostEnqueuePromiseJob ( job, realm )
 //-----------------------------------------------------------------------------
 
+/**
+ * Queues a new pledge job as a microtask to execute later. For simplicity,
+ * I've omitted the realm functionality.
+ * @param {Function} job The function to execute. 
+ * @returns {void}
+ */
 export function hostEnqueuePledgeJob(job) {
     queueMicrotask(() => {
         job.call();
     });
 }
 
+//-----------------------------------------------------------------------------
+// 25.6.2.2 NewPromiseResolveThenableJob ( promiseToResolve, thenable, then )
+//-----------------------------------------------------------------------------
+
+/**
+ * Represents a job to resolve a thenable. I opted to use a class here to make
+ * it more idiomatic to JavaScript even though the return value is a function.
+ */
 export class PledgeResolveThenableJob {
     constructor(pledgeToResolve, thenable, then) {
         return () => {
@@ -33,6 +47,14 @@ export class PledgeResolveThenableJob {
     }
 }
 
+//-----------------------------------------------------------------------------
+// 25.6.2.1 NewPromiseReactionJob ( reaction, argument )
+//-----------------------------------------------------------------------------
+
+/**
+ * Represents a job to run a reaction. I opted to use a class here to make
+ * it more idiomatic to JavaScript even though the return value is a function.
+ */
 export class PledgeReactionJob {
     constructor(reaction, argument) {
         return () => {
@@ -55,5 +77,4 @@ export class PledgeReactionJob {
             }
         };
     }
-
 }
